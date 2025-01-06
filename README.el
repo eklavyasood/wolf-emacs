5635f9d6abe6e716ade14eec38e4869b428f580c
eklavya <eklavyasood@gmail.com>
2025-01-06 19:05:23 +0530

    added vterm and vterm-toggle

--- README.el
+++ README.el
@@ -285,6 +285,7 @@
   "t"  '(:ignore t :wk "Toggle")
   "t l"  '(display-line-numbers-mode :wk "Toggle line numbers")
   "t n"  '(neotree-toggle :wk "Toggle neotree")
+  "t v"  '(vterm-toggle :wk "Toggle vterm")
 )
 
 (wolf/leader
@@ -359,6 +360,31 @@
       "f U" '(sudo-edit :wk "Sudo edit file")
     )
 )
+
+(use-package vterm
+  :config
+    (setq shell-file-name "zsh"
+	    vterm-shell "zsh"
+          vterm-max-scrollback 5000))
+
+(use-package vterm-toggle
+  :after vterm
+  :config
+    (setq vterm-toggle-fullscreen-p nil)
+    (setq vterm-toggle-scope 'project)
+    (add-to-list 'display-buffer-alist
+		 '((lambda (buffer-or-name _)
+		     (let ((buffer (get-buffer buffer-or-name)))
+		       (with-current-buffer buffer
+			 (or (equal major-mode 'vterm-mode)
+			     (string-prefix-p vterm-buffer-name (buffer-name buffer))))))
+		   (display-buffer-reuse-window display-buffer-in-direction)
+		   ;;(display-buffer-reuse-window display-buffer-in-direction)
+		   ;;display-buffer-in-direction/direction/dedicated is added in emacs27
+		   ;;(direction . bottom)
+		   ;;(dedicated . t) ;dedicated is supported in emacs27
+		   (reusable-frames . visible)
+		   (window-height . 0.3))))
 
 (use-package which-key
   :init
